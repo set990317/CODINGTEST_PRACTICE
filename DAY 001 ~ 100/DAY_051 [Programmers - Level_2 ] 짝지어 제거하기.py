d5a6d4ea87b388e91d216711971550ad320a3c32
tteum <set990317@naver.com>
2022-05-11 23:34:01 +0900

    upload

---
+++ DAY 001 ~ 100/DAY_051 [Programmers - Level_2 ] 짝지어 제거하기.py
@@ -0,0 +1,20 @@
+# [프로그래머스 Level 1] - 짝지어 제거하기
+# 블로그 주소 : https://tteum.tistory.com/184
+
+def solution(s):
+    answer = -1
+    stack = []
+    
+    for i in range(len(s)):
+        if len(stack) != 0 :
+            if stack[-1] == s[i]:
+                stack.pop()
+            else :
+                stack.append(s[i])
+        else : stack.append(s[i])
+    
+    if len(stack) == 0:
+        answer = 1
+    else : answer = 0
+    
+    return answer
