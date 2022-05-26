a2ab6bfcc121ba45a9c42de786e5f0142c78e1ad
tteum <set990317@naver.com>
2022-05-27 02:17:53 +0900

    upload

--- DAY 001 ~ 100/DAY_066 [백준] 11022번 - A+B - 8.py
+++ DAY 001 ~ 100/DAY_066 [백준] 11022번 - A+B - 8.py
@@ -1,3 +1,6 @@
+# [백준] 11022번 - A+B - 8
+# 블로그 주소 : https://tteum.tistory.com/200
+
 import sys
 
 T = int(sys.stdin.readline())
---
+++ DAY 001 ~ 100/DAY_067 [백준] 2438번 - 별 찍기 - 1.py
@@ -0,0 +1,7 @@
+# [백준] 2438번 - 별 찍기 - 1
+# 블로그 주소 : https://tteum.tistory.com/201
+
+n = int(input())
+
+for i in range(1,n+1):
+    print("*" * i)
