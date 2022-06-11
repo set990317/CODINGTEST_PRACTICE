c7d534af9d6c8b9f54ccaa1208f1fa292f4e2e47
tteum <set990317@naver.com>
2022-06-11 18:46:48 +0900

    upload

---
+++ DAY 001 ~ 100/DAY_082 [백준] 1065번 - 한수.py
@@ -0,0 +1,26 @@
+# [백준] 1065번 - 한수
+# 블로그 주소 : https://tteum.tistory.com/216
+
+def hansu(num):
+    arr = list(map(int,str(num)))
+    d = arr[1] - arr[0]
+    for i in range(len(arr)-1,0,-1):
+        tmp = arr[i] - arr[i-1]
+        if tmp != d :
+            return False
+    return True
+
+def main():
+    n = int(input())
+    count = 0
+
+    for num in range(1,n+1):
+        if num < 100 :
+            count += 1
+        else :
+            result = hansu(num)
+            if result == True:
+                count += 1
+    print(count)
+
+main()
