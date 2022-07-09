1765e313b1390225499ac24daaccb457917d5078
tteum <set990317@naver.com>
2022-07-09 23:08:51 +0900

    upload

---
+++ DAY 101 ~ 200/DAY_109 [코드업] 6084 - 소리 파일 저장용량 계산하기.py
@@ -0,0 +1,8 @@
+# [코드업] 6084 - 소리 파일 저장용량 계산하기
+# 블로그 주소 : https://tteum.tistory.com/246
+
+import sys
+
+h,b,c,s = map(int,sys.stdin.readline().split())
+result = round( h*b*c*s / 8 / 1024 / 1024, 1)
+print(result, "MB")
