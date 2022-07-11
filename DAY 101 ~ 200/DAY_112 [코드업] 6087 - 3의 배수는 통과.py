ecabc7d537101d79892b5b8a3eea7712d695a6f4
tteum <set990317@naver.com>
2022-07-12 01:19:43 +0900

    upload

---
+++ DAY 101 ~ 200/DAY_112 [코드업] 6087 - 3의 배수는 통과.py
@@ -0,0 +1,10 @@
+# [코드업] 6087 - 3의 배수는 통과
+# 블로그 주소 : https://tteum.tistory.com/252
+
+import sys
+
+N = int(sys.stdin.readline())
+
+for i in range(1,N+1):
+    if i % 3 != 0 :
+        print(i, end=' ')
