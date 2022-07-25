4042725963a469a46eead4f7cbe1cb7a45e0f2fb
tteum <set990317@naver.com>
2022-07-25 18:04:44 +0900

    upload

---
+++ DAY 101 ~ 200/DAY_125 [백준] 1026번 - 보물.py
@@ -0,0 +1,18 @@
+# [코드업] 1026번 - 보물
+# 블로그 주소 : https://tteum.tistory.com/268
+
+import sys
+
+n = int(input())
+A = list(map(int, sys.stdin.readline().split()))
+B = list(map(int, sys.stdin.readline().split()))
+
+result = 0
+
+A.sort()
+
+for i in range(n):
+    result += A[i] * max(B)
+    B.remove(max(B))
+    
+print(result)
