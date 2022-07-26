49161635ecc8a26f16b0a3efeed0abccb9dac0c0
tteum <set990317@naver.com>
2022-07-26 16:47:44 +0900

    upload

---
+++ DAY 101 ~ 200/DAY_126 [백준] 10872번 - 팩토리얼.py
@@ -0,0 +1,11 @@
+# [코드업] 10872번 - 팩토리얼
+# 블로그 주소 : https://tteum.tistory.com/270
+
+def factorial(num):
+    if num > 1:
+        return num * factorial(num-1)
+    else : 
+        return 1
+
+n = int(input())
+print(factorial(n))
