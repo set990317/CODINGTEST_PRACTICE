c882c07c69d5f05875af2505b11eb6569744de78
tteum <set990317@naver.com>
2022-07-27 14:16:47 +0900

    upload

---
+++ DAY 101 ~ 200/DAY_127 [백준] 10870번 - 피보나치 수 5.py
@@ -0,0 +1,13 @@
+# [코드업] 10870번 - 피보나치 수 5
+# 블로그 주소 : https://tteum.tistory.com/272
+
+def fibo(num):
+    if num == 0 :
+        return 0
+    elif num == 1 :
+        return 1
+    else :
+        return fibo(num-1) + fibo(num-2)
+
+n = int(input())
+print(fibo(n))
