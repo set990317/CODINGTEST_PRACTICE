534757cea7b59aafced76fdbaa2913d3b8ff5ade
tteum <set990317@naver.com>
2022-07-29 17:57:17 +0900

    upload

---
+++ DAY 101 ~ 200/DAY_129 [백준] 2864번 - 5와 6의 차이.py
@@ -0,0 +1,17 @@
+# [코드업] 2864번 - 5와 6의 차이
+# 블로그 주소 : https://tteum.tistory.com/278
+
+
+import sys
+
+a, b = map(str, sys.stdin.readline().split())
+
+a = a.replace('6','5')
+b = b.replace('6','5')
+min = int(a) + int(b)
+
+a = a.replace('5','6')
+b = b.replace('5','6')
+max = int(a) + int(b)
+
+print(min,max)
