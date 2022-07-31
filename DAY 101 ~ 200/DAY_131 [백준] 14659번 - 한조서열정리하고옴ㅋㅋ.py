ba0c01abe9efda2a6b8c859e8539c0563682fb56
tteum <set990317@naver.com>
2022-07-31 18:16:21 +0900

    upload

---
+++ DAY 101 ~ 200/DAY_131 [백준] 14659번 - 한조서열정리하고옴ㅋㅋ.py
@@ -0,0 +1,21 @@
+# [코드업] 14659번 - 한조서열정리하고옴ㅋㅋ
+# 블로그 주소 : https://tteum.tistory.com/280
+
+import sys
+N = int(sys.stdin.readline())
+hanzo = list(map(int, sys.stdin.readline().split()))
+winner = hanzo[0]
+result = []
+count = 0
+
+for i in range(1,len(hanzo)):
+    if winner < hanzo[i]:
+        winner = hanzo[i]
+        result.append(count)
+        count = 0
+    else :
+        count += 1
+        if i == len(hanzo)-1:
+            result.append(count)
+            
+print(max(result))
