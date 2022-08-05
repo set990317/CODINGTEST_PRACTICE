2652d6d4dd2962316b4b843f20a2fb963e9838de
tteum <set990317@naver.com>
2022-08-05 23:34:00 +0900

    upload

---
+++ DAY 101 ~ 200/DAY_136 [백준] 1193번 - 분수찾기.py
@@ -0,0 +1,17 @@
+# [코드업] 2720번 - 세탁소 사장 동혁
+# 블로그 주소 : https://tteum.tistory.com/285
+
+import sys
+
+T = int(sys.stdin.readline())
+money = [25, 10, 5, 1]
+for _ in range(T):
+    C = int(sys.stdin.readline())
+    result = []
+    for num in money:
+        result.append(C // num)
+        C = C % num
+
+
+    for i in range(len(result)):
+        print(result[i], end = ' ')
