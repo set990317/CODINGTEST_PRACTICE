d87a210db889a22091905d475a6e0863cddf8298
tteum <set990317@naver.com>
2022-08-13 06:53:00 +0900

    upload

---
+++ DAY 101 ~ 200/DAY_143 [백준] 10156번 - 과자.py
@@ -0,0 +1,13 @@
+# [코드업] 10156번 - 과자
+# 블로그 주소 : https://tteum.tistory.com/292
+
+import sys
+
+snack, n, money = map(int,sys.stdin.readline().split())
+
+a = ( snack * n ) - money
+
+if a <= 0 :
+    print(0)
+else :
+    print(a)
