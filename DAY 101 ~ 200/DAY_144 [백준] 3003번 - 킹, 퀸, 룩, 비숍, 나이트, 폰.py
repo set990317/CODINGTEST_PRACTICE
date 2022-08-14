7499e68dd4648396e9b62e08d1b8bcfbc7bb9bf6
tteum <set990317@naver.com>
2022-08-14 22:29:02 +0900

    upload

---
+++ DAY 101 ~ 200/DAY_144 [백준] 3003번 - 킹, 퀸, 룩, 비숍, 나이트, 폰.py
@@ -0,0 +1,10 @@
+# [코드업] 3003번 - 킹, 퀸, 룩, 비숍, 나이트, 폰
+# 블로그 주소 : https://tteum.tistory.com/293
+
+import sys
+
+chess = list(map(int,sys.stdin.readline().split()))
+answer = [1,1,2,2,2,8]
+
+for i in range(len(chess)):
+    print(answer[i]-chess[i], end =' ')
