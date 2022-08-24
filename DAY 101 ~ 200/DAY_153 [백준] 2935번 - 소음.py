c85b9c726d68b4ead9a6e5e30b8bd1f117b83e98
tteum <set990317@naver.com>
2022-08-24 23:36:57 +0900

    upload

---
+++ DAY 101 ~ 200/DAY_153 [백준] 2935번 - 소음.py
@@ -0,0 +1,11 @@
+# [백준] 2935번 - 소음
+# 블로그 주소 : https://tteum.tistory.com/306
+
+a = int(input())
+operator = str(input())
+b = int(input())
+
+if operator == '+':
+    print(a+b)
+else :
+    print(a*b)
