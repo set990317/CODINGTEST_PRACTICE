c74e6b36c1c3acf762abe6d6a679e95548850238
tteum <set990317@naver.com>
2022-08-27 23:59:59 +0900

    modified

--- DAY 101 ~ 200/DAY_156 [백준] 2576번 - 홀수.py
+++ DAY 101 ~ 200/DAY_156 [백준] 2576번 - 홀수.py
@@ -1,3 +1,18 @@
 # [백준] 2576번 - 홀수
 # 블로그 주소 : https://tteum.tistory.com/309
 
+tmp = []
+odd = []
+
+for _ in range(7):
+    tmp.append(int(input()))
+
+for num in tmp:
+    if num % 2 != 0 :
+        odd.append(num)
+
+if len(odd) != 0:
+    print(sum(odd))
+    print(min(odd))
+else :
+    print(-1)
