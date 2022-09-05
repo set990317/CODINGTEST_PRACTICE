cc173a69c2a903a4bf6287981d9170f60a16e7d0
tteum <set990317@naver.com>
2022-09-05 18:14:01 +0900

    upload

---
+++ DAY 101 ~ 200/DAY_165 [백준] 1032번 - 명령 프롬프트.py
@@ -0,0 +1,22 @@
+# [백준] 2455번 - 지능형 기차
+# 블로그 주소 : https://tteum.tistory.com/319
+
+import sys
+
+total = 0
+max = 0
+
+for _ in range(4):
+    a, b = map(int, sys.stdin.readline().split())
+
+    total = total - int(a)
+
+    if max < total :
+        max = total
+
+    total = total + int(b)
+
+    if max < total :
+        max = total
+
+print(max)
