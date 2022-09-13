387f97ec268920996d986cef64b1d1c365cbc96f
tteum <set990317@naver.com>
2022-09-13 20:11:04 +0900

    upload

--- DAY 101 ~ 200/DAY_172 [백준] 11557번 - Yangjojang of The Year.py
+++ DAY 101 ~ 200/DAY_172 [백준] 11557번 - Yangjojang of The Year.py
@@ -1,2 +1,5 @@
-# [백준] 11557번 - Yangjojang of The Year
-# 블로그 주소 : https://tteum.tistory.com/328
+# [백준] 10718번 - We love Kriii
+# 블로그 주소 : https://tteum.tistory.com/329
+
+print("강한친구 대한육군")
+print("강한친구 대한육군")
