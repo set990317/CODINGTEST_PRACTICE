8f90a3a14773b18977e8a4b796ba183f04370f33
tteum <set990317@naver.com>
2022-09-23 23:02:31 +0900

    DAY 183 upload

---
+++ DAY 101 ~ 200/DAY_183 [Programmers - Level_2 ] 올바른 괄호.py
@@ -0,0 +1,23 @@
+# [프로그래머스 Level 2] - 올바른 괄호
+# 블로그 주소 : https://tteum.tistory.com/359
+
+def solution(s):
+    answer = True
+    stack = []
+    
+    for i in range(len(s)):
+        if s[i] == "(":
+            stack.append("(")
+        else:
+            if len(stack) != 0:
+                if stack[-1] == "(":
+                    stack.pop()
+                else :
+                    stack.append(")")
+            else :
+                stack.append(")")                
+    
+    if len(stack) != 0 :
+        answer = False
+    
+    return answer
